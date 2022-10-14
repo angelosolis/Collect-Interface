a6a0a0fad8cf8ded62acc483131ce45b370d0603
angelosolis <88774848+angelosolis@users.noreply.github.com>
2022-10-14 23:23:58 +0800

    Create Collect.java

---
+++ Collect.java
@@ -0,0 +1,10 @@
+//@author Angelo Solis
+
+public interface Collect
+    {
+       public void enqueue(int x);
+       public void dequeue();
+       public int getFront();
+       public int getRear();
+       public void print();
+    }
